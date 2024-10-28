87374e0fe67e5cdd776ed6dd192dd9f5263d0636
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-10-28 07:48:46 +0100

    perf: move api to pages/api

--- apps/nextjs-app/src/pages/api/[[...route]].ts
+++ apps/nextjs-app/src/pages/api/[[...route]].ts
@@ -1,5 +1,7 @@
 import { handle } from '@hono/node-server/vercel';
+import { swaggerUI } from '@hono/swagger-ui';
 import { Hono } from 'hono';
+import { createOpenApiDocument } from 'hono-zod-openapi';
 
 import { productRouter } from '@/features/products/server/product.router';
 import { workshopRouter } from '@/features/workshop/workshop.router';
@@ -10,7 +12,7 @@ export const config = {
   },
 };
 
-const app = new Hono().basePath('/api/legacy');
+const app = new Hono().basePath('/api');
 
 app.get('/hello', (c) => {
   return c.json({
@@ -20,5 +22,16 @@ app.get('/hello', (c) => {
 
 app.route('/workshop', workshopRouter);
 app.route('/products', productRouter);
+
+createOpenApiDocument(app, {
+  info: {
+    title: 'Workshop Node SQL Server Api',
+    version: '1.0.0',
+  },
+});
+
+app.get('/documentation', swaggerUI({ url: '/api/doc' }));
+
+export type HonoLocalAppType = typeof app;
 
 export default handle(app);
