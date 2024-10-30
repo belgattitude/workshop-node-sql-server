0c2445e9621c5b83cfce787f5e48145cdd835fd7
Sebastien Vanvelthem <sebastien.vanvelthem@weareretail.ai>
2024-10-30 11:49:25 +0100

    feat: prep example redux

--- apps/nextjs-app/src/features/products/api/search-brands.ts
+++ apps/nextjs-app/src/features/products/api/search-brands.ts
@@ -2,5 +2,5 @@ import { localApiFetcher } from '@/config/local-api-fetcher.config';
 import type { ProductRepoSearchData } from '@/features/products/server/product.repo';
 
 export const searchBrands = async () => {
-  return localApiFetcher.get<ProductRepoSearchData>('/products/search').json();
+  return localApiFetcher.get<ProductRepoSearchData>('/brands/search').json();
 };
