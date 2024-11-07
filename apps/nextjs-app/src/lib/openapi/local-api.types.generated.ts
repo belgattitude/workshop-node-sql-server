30bf1368ed8788d6287c3ba029e74dcef9c124bf
Sebastien Vanvelthem <sebastien.vanvelthem@weareretail.ai>
2024-11-07 09:39:45 +0100

    chore(deps): to latest

--- apps/nextjs-app/src/lib/openapi/local-api.types.generated.ts
+++ apps/nextjs-app/src/lib/openapi/local-api.types.generated.ts
@@ -281,6 +281,61 @@ export interface paths {
     patch?: never;
     trace?: never;
   };
+  '/api/brands/search': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    get: {
+      parameters: {
+        query?: {
+          limit?: number;
+          searchName?: string;
+        };
+        header?: never;
+        path?: never;
+        cookie?: never;
+      };
+      requestBody?: never;
+      responses: {
+        /** @description 200 OK */
+        200: {
+          headers: {
+            [name: string]: unknown;
+          };
+          content: {
+            'application/json': {
+              success: boolean;
+              data?: {
+                id: number;
+                name: string;
+              }[];
+              error?: {
+                message: string;
+              };
+              meta?: {
+                affectedRows?: number;
+                timeMs?: number;
+                query?: {
+                  sql?: string;
+                  params?: unknown[];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    put?: never;
+    post?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
 }
 export type webhooks = Record<string, never>;
 export interface components {
