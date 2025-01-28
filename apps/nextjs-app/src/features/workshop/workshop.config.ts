915bf777fd8a2b2b23172e43ca2a4b24e5469c9a
Sebastien Vanvelthem <sebastien.vanvelthem@weareretail.ai>
2025-01-28 12:12:10 +0100

    chore(deps): to latest

--- apps/nextjs-app/src/features/workshop/workshop.config.ts
+++ apps/nextjs-app/src/features/workshop/workshop.config.ts
@@ -1,5 +1,5 @@
 import { WorkshopRepo } from '@/features/workshop/workshop.repo';
-import { workshopDatasource } from '@/server/config/datasource.kysely-sqlserver.config';
+import { workshopDatasource } from '@/server/config/ds.kysely-sqlserver.config';
 
 export const workshopRepo = new WorkshopRepo({
   ds: workshopDatasource,
